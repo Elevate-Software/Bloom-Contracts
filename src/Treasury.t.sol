976160cbaf1258a3f4a2f15a29da8c39d6817829
Chase Brown <Chasebrown7@icloud.com>
2022-07-05 16:16:26 -0700

    deleted unnecessary functions and renamed contract to Treasury

---
+++ src/Treasury.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.6;
+
+import "../lib/ds-test/src/test.sol";
+
+import "./Treasury.sol";
+
+contract BloomContractsTest is DSTest {
+    Treasury treasury;
+
+    function setUp() public {
+        treasury = new Treasury();
+    }
+
+}
