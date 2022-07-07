46b40cad08fb2a8583e35dd799297949188af1db
Chase Brown <Chasebrown7@icloud.com>
2022-07-07 14:24:48 -0700

    Scaffolding for Treasury and SwapInterface

--- src/SwapInterface.sol
+++ src/SwapInterface.sol
@@ -3,6 +3,74 @@ pragma solidity ^0.8.6;
 
 import "./OpenZeppelin/Ownable.sol";
 
+// Curve Docs: https://curve.readthedocs.io/
+
+/// @dev    This contract allows investors to invest ETH, DAI, USDT, or USDC.
+///         This contract uses CRV protocol to swap investments to a specific stablecoin.
+///         This contract will send all stablecoin to the Treasury for accounting.
+///         Can be disabled/enabled by an admin.
+///         Only whitelisted wallets can invest into the protocol.
+///         To Be Determinted:
+///          - Take a fee?
 contract SwapInterface is Ownable{
-    
+
+    // ---------------
+    // State Variables
+    // ---------------
+
+    address public stableCurrency;   /// @notice Used to store address of coin used to deposit/payout from Treasury.
+    bool public contractEnabled;     /// @notice Bool for contract enabling / disabling investments
+
+    mapping(address => InvestmentData) private investmentTracker;   /// @notice Tracks wallets and their individual investments made.
+    mapping(address => bool) public whitelistedToken;               /// @notice Whitelist for coins accepted for investment.
+    mapping(address => bool) public whitelistedWallet;              /// @notice Whitelist for wallets allowed to invest.
+
+    /// @notice Struct used to store important data for each investment made before sent to the treasury.
+    /// @param
+    /// @param
+    /// @param
+    /// @param
+    struct InvestmentData {
+        address currency;
+        uint amountInvested;
+        uint stableCoinEquivalent;
+        uint timeUnix;
+    }
+
+    // -----------
+    // Constructor
+    // -----------
+
+    /// @notice Initializes Treasury.sol 
+    /// @param _stableCurrency Used to store address of stablecoin used in contract (default is USDC).
+    constructor (
+        address _stableCurrency
+    ) {
+        stableCurrency = _stableCurrency;
+        transferOwnership(msg.sender);
+    }
+
+    // ------
+    // Events
+    // ------
+
+    // TODO: Add any necessary events.
+
+    // ---------
+    // Modifiers
+    // ---------
+
+    // TODO: isAuthorized modifier goes here.
+
+    // ---------
+    // Functions
+    // ---------
+
+    // TODO: 
+    // add wallet to whitelist
+    // invest()
+    // swap function -> uses CRV
+    // change stableCurrency -> onlyOwner
+    // disable/enable contract -> onlyOwner
+    // update token whitelist
 }
--- src/Treasury.sol
+++ src/Treasury.sol
@@ -3,6 +3,123 @@ pragma solidity ^0.8.6;
 
 import "./OpenZeppelin/Ownable.sol";
 
+/// @dev    The Treasury contract will be the focal point within the protocol.
+///         This contract will keep track of all accounting
+///         It will account for all assets, investors, and dividends for all
+///         projects within the Bloom Project.
+///         This contract calls mint() on BloomToken.sol
+///         This contract recieves USDC or other stablecoins from SwapInterface.sol
+///         This contract tracks all BTC invested into the Circle acc. updateBtcReceived() called by bot.
+///         This contract also can send USDC or other stablecoin to our Circle acc.
+///         To be determined:
+///          - What values we need to return for front end?
+///          - Are we keeping track of projects or just investments?
 contract Treasury is Ownable {
-    
+
+    // ---------------
+    // State Variables
+    // ---------------
+
+    address public stableCurrency;                               /// @notice Used to store address of coin used to deposit/payout from Treasury.
+    mapping(address => InvestorData) private investorLibrary;    /// @notice Mapping of Investor wallets to their investment data held in InvestorData.
+    mapping(address => bool) public isAuthorizedUser;            /// @notice isAuthorizedUser[address] returns true if wallet is authorized;
+
+    /// @notice Investor srtruct is used to track data points of investments made by investors.
+    /// @param amountInvested Tracks USD-stablecoin equivalent to investment made.
+    /// @param dividendsReceived Tracks dividends received by investor in stablecoin.
+    struct InvestorData {
+        uint totalAmountInvested;
+        uint totalDividendsReceived;
+        InvestmentReceipt[] investmentLibrary;
+        DividendReceipt[] dividendLibrary;
+    }
+
+    /// TODO: Will
+    /// @notice this does this
+    /// @param 
+    /// @param
+    struct InvestmentReceipt {
+        uint amountInvested;
+        uint timeUnix;
+    }
+
+    /// TODO: Marcus
+    struct DividendReceipt {
+        uint dividendPaid;
+        uint timeUnix;
+    }
+
+    // -----------
+    // Constructor
+    // -----------
+
+    /// @notice Initializes Treasury.sol 
+    /// @param _stableCurrency Used to store address of stablecoin used in contract (default is USDC).
+    constructor (
+        address _stableCurrency
+    ) {
+        stableCurrency = _stableCurrency;
+
+        transferOwnership(msg.sender);
+        isAuthorizedUser[msg.sender] = true;
+    }
+
+    // ------
+    // Events
+    // ------
+
+    // TODO: Add any necessary events.
+
+    // ---------
+    // Modifiers
+    // ---------
+
+    // TODO: isAuthorized modifier goes here.
+
+    // ---------
+    // Functions
+    // ---------
+
+    /// @notice Withdraw stableCurrency from contract to a Circle acc.
+    function withdrawToCircle() public {
+
+    }
+
+    /// @notice Updates investment values when a BTC investment is made.
+    /// @dev    Function called by python bot.
+    function updateBtcReceived() public {
+
+    }
+
+    /// @notice Updates investment values when an investment is made through Dapp.
+    /// @dev    Should only be called by SwapInterface.sol.
+    function updateStableReceived() public {
+
+    }
+
+    /// @notice Mints BLOOM tokens to a certain investor.
+    /// @dev    Calls BloomToken.sol::mint().
+    function mintBloom() public {
+
+    }
+
+    /// @notice Allows an admin (owner) to add/remove an authorized user.
+    function updateAuthorizedUser() public onlyOwner() {
+
+    }
+
+    // ~ View Functions ~
+
+    /// @notice Should return contract balance of stableCurrency.
+    /// @return uint Amount of stableCurrency is inside contract.
+    function balanceOfStableCurrency() public view returns (uint) {
+
+    }
+
+    /// TODO: Add necessary view functions.
+    /// @dev Think about what values in here are important that need to be viewed later on.
+
+    /// TODO: Setup contract for dividend payouts.
+    /// TODO: add updateStableCurrency variable.
+    /// TODO: have a safeWithdraw
 }
