9d06ca4e2a9b045e7bcdfb37c50c5875335b0584
Chase Brown <Chasebrown7@icloud.com>
2022-07-19 13:28:36 -0700

    Separation in actor functions

--- src/users/Actor.sol
+++ src/users/Actor.sol
@@ -19,10 +19,30 @@ contract Actor {
     /*** TRY FUNCTIONS ***/
     /*********************/
 
-    // function try_transferToken(address token, address to, uint256 amt) external returns (bool ok) {
-    //     string memory sig = "transfer(address,uint256)";
-    //     (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amt));
-    // }
+    // ~ BloomToken ~
+
+    function try_transferToken(address token, address to, uint256 amount) external returns (bool ok) {
+        string memory sig = "transfer(address,uint256)";
+        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
+    }
+
+    function try_transferFromToken(address token, address from, address to, uint256 amount) external returns (bool ok) {
+        string memory sig = "transferFrom(address,address,uint256)";
+        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
+    }
+
+    function try_approveToken(address token, address to, uint256 amount) external returns (bool ok) {
+        string memory sig = "approve(address,uint256)";
+        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
+    }
+
+    function try_updateException(address token, address wallet, bool isException) external returns (bool ok) {
+        string memory sig = "updateException(address,bool)";
+        (ok,) = address(token).call(abi.encodeWithSignature(sig, wallet, isException));
+
+    }
+
+    // ~ swapInterface ~
 
     function try_updateStableReceived(address treasury, address _wallet, uint256 _amount, uint256 _timeUnix) external returns (bool ok) {
         string memory sig = "updateStableReceived(address,uint256,uint256)";
@@ -72,31 +92,11 @@ contract Actor {
     function try_updateTokenWhitelist(address swapInterface, address _tokenAddress, bool _allowed) external returns (bool ok) {
         string memory sig = "updateTokenWhitelist(address,bool)";
         (ok,) = address(swapInterface).call(abi.encodeWithSignature(sig, _tokenAddress, _allowed));
+    }
         
     function try_addAuthorizedUser(address treasury, address wallet) external returns (bool ok) {
         string memory sig = "addAuthorizedUser(address)";
         (ok,) = address(treasury).call(abi.encodeWithSignature(sig, wallet));
-    }
-
-    function try_transferToken(address token, address to, uint256 amount) external returns (bool ok) {
-        string memory sig = "transfer(address,uint256)";
-        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
-    }
-
-    function try_transferFromToken(address token, address from, address to, uint256 amount) external returns (bool ok) {
-        string memory sig = "transferFrom(address,address,uint256)";
-        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
-    }
-
-    function try_approveToken(address token, address to, uint256 amount) external returns (bool ok) {
-        string memory sig = "approve(address,uint256)";
-        (ok,) = address(token).call(abi.encodeWithSignature(sig, to, amount));
-    }
-
-    function try_updateException(address token, address wallet, bool isException) external returns (bool ok) {
-        string memory sig = "updateException(address,bool)";
-        (ok,) = address(token).call(abi.encodeWithSignature(sig, wallet, isException));
-
     }
     
 }
