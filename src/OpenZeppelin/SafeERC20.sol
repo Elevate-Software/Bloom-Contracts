2ea3acf3492e57508e55ae6521fd31a6f0c04580
wdlord <44736323+wdlord@users.noreply.github.com>
2022-08-11 14:19:38 -0700

    consolidated solidity versions in OpenZeppelin contracts

--- src/OpenZeppelin/SafeERC20.sol
+++ src/OpenZeppelin/SafeERC20.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.7.0) (token/ERC20/utils/SafeERC20.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.6;
 
 // NOTE: I changed the imports - Will
 import { IERC20 } from "../interfaces/InterfacesAggregated.sol";
