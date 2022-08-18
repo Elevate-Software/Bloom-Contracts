78bc0ae0a56ffbef9bec99a57ae32022d4fbdf47
wdlord <44736323+wdlord@users.noreply.github.com>
2022-08-18 14:30:54 -0700

    invest test working

--- src/tests/BloomMainDeployment.t.sol
+++ src/tests/BloomMainDeployment.t.sol
@@ -84,6 +84,51 @@ contract BloomMainDeploymentTest is DSTest, Utility {
     }
 
     function test_BloomMainDeployment_invest() public {
+
+        uint256 swapAmount = 10 ether;
+
+        // Allow DAI to be swapped setup.
+        dev.try_updateTokenWhitelist(address(swapInterface), DAI, true);
+        dev.try_addWalletToWhitelist(address(swapInterface), address(bob));
+        mint("DAI", address(bob), swapAmount);
+
+        // pre-state checks
+        // contracts should have no money invested.
+        assertEq(IERC20(USDC).balanceOf(swapInterface.Treasury()), 0 * USD);
+        assertEq(IERC20(DAI).balanceOf(address(bob)), swapAmount);
+
+        assertEq(treasury.getInvestorData(address(bob)).totalAmountInvested, 0);
+        assertEq(treasury.getInvestmentLibrary(address(bob)).length, 0);
+        assertEq(treasury.getDividendLibrary(address(bob)).length, 0);
+
+        assertEq(IERC20(address(bloomToken)).totalSupply(), 0);
+        assertEq(IERC20(address(bloomToken)).balanceOf(address(bob)), 0);
+
+        // state change
+        // investor Bob calls SwapInterface.invest() to invest in the REIT.
+        assert(bob.try_approveToken(DAI, address(swapInterface), swapAmount));
+        assert(bob.try_invest(address(swapInterface), DAI, swapAmount));
+
+        // post-state checks
+        // contracts should reflect that money has been invested.
+        uint256 amountReceived = IERC20(USDC).balanceOf(swapInterface.Treasury());
+        assertGt(amountReceived, 0);
+
+        assertEq(IERC20(USDC).balanceOf(address(swapInterface)), 0 * USD);
+
+        assertEq(treasury.getInvestorData(address(bob)).totalAmountInvested, amountReceived);
+        assertEq(treasury.getInvestmentLibrary(address(bob)).length, 1);
+        assertEq(treasury.getDividendLibrary(address(bob)).length, 0);
+
+        assertEq(treasury.getInvestmentLibrary(address(bob))[0].amountInvested, amountReceived);
+        assertEq(treasury.getInvestmentLibrary(address(bob))[0].timeUnix, block.timestamp);
+
+        assertEq(IERC20(address(bloomToken)).totalSupply(), amountReceived * 10**12);
+        assertEq(IERC20(address(bloomToken)).balanceOf(address(bob)), amountReceived * 10**12);
+
+    }
+
+    function test_BloomMainDeployment_investETH() public {
         
     }
 
